8c8e5c3ce40ebb3f8f7bd31122135fa2aee23042
JasonSpookBartlett <jason.spook.bartlett@gmail.com>
2026-09-07 15:13:52 +1200

    Add files via upload

---
+++ README_INSTALL.txt
@@ -0,0 +1,19 @@
+RACKMATE V1.32 PWA INSTALL
+
+Upload ALL of these to the ROOT of the GitHub RackMate repo:
+- index.html
+- manifest.webmanifest
+- sw.js
+- icons/icon-192.png
+- icons/icon-512.png
+
+Keep the icons folder as a folder.
+
+After GitHub Pages updates:
+1. Open https://jasonspookbartlett.github.io/RackMate/ in Chrome on Android.
+2. Reload and confirm the screen says V1.32.
+3. Chrome ⋮ menu -> Install app (or Add to Home screen).
+4. Launch RackMate from the new Home Screen icon.
+5. It should open standalone in landscape without Chrome's address bar.
+
+If you already made an older RackMate shortcut, remove it before installing this build.
--- sw.js
+++ sw.js
@@ -1,1 +1,35 @@
-self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('fetch',e=>{});
+const CACHE='rackmate-v1.32';
+const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP_SHELL)));
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  if(event.request.mode==='navigate'){
+    event.respondWith(
+      fetch(event.request).then(res=>{
+        const copy=res.clone();
+        caches.open(CACHE).then(c=>c.put('./index.html',copy));
+        return res;
+      }).catch(()=>caches.match('./index.html'))
+    );
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then(cached=>cached||fetch(event.request).then(res=>{
+      const copy=res.clone();
+      caches.open(CACHE).then(c=>c.put(event.request,copy));
+      return res;
+    }))
+  );
+});
