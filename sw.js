36edd468113a2c642c22629598b8af2cdc2e951b
JasonSpookBartlett <jason.spook.bartlett@gmail.com>
2026-09-12 18:01:33 +1200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='rackmate-v1.53';
+const CACHE='rackmate-v1.54';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 
 self.addEventListener('install',event=>{
